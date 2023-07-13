7200f7129d9cc99e7630cb9a8a1203e70c5bc767
sgp1986 <sgp1229@gmail.com>
2023-07-13 15:22:41 -0600

    update bepb as phonebook backend

--- full_stack/bepb/index.js
+++ full_stack/bepb/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan('tiny', { 
