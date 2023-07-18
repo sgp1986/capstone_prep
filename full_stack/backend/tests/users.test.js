1f60f1fbaef2f698296ab2698d528198e9fa6f95
sgp1986 <sgp1229@gmail.com>
2023-07-18 17:05:26 -0600

    added user credentials to backend

---
+++ full_stack/backend/tests/users.test.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const helper = require('./test_helper');
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash});
+
+    await user.save();
+  });
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'sprice',
+      name: 'Steve Price',
+      password: 'calliebear',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+  });
+})
